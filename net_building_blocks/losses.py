a3f0ad3ca742fe33bad74a21283c1850b22fa082
yinpenghhz@hotmail.com <yinpenghhz@hotmail.com>
2021-10-15 16:17:00 +0800

    exp nll

---
+++ net_building_blocks/losses.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+
+def exponential_nll_loss(lam, label):
+    """
+    Args:
+        lam:[B, 1] or [B] lambda of exponential distribution. lam must be bigger than 0 !!!!
+        label: [B, 1] or [B] x of exponential distribution
+    Returns: [B, 1] or [B] loss
+    """
+    lam = tf.clip_by_value(lam, clip_value_min=1e-6, clip_value_max=None)
+    return lam * label * tf.log(lam)
