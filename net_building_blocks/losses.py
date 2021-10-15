5a0eb646accece986458279673ab53758677045b
yinpenghhz@hotmail.com <yinpenghhz@hotmail.com>
2021-10-15 19:43:31 +0800

    inverse gaussian

--- net_building_blocks/losses.py
+++ net_building_blocks/losses.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 
@@ -10,3 +11,19 @@ def exponential_nll_loss(lam, label):
     """
     lam = tf.clip_by_value(lam, clip_value_min=1e-6, clip_value_max=1e6)
     return lam * label - tf.log(lam)
+
+
+def inverse_gaussian_nll_loss(lam, mu, label):
+    """
+    https://zh.wikipedia.org/wiki/%E9%80%86%E9%AB%98%E6%96%AF%E5%88%86%E5%B8%83
+    Args:
+        lam: [B, 1] inverse gaussian distribution lambda
+        mu: [B, 1] inverse gaussian distribution mu
+        label:  [B, 1] inverse gaussian distribution
+    lam>0, mu>0, label>0
+    Returns:[B, 1]
+    """
+    lam = tf.clip_by_value(lam, clip_value_min=1e-6, clip_value_max=1e6)
+    mu = tf.clip_by_value(mu, clip_value_min=1e-6, clip_value_max=1e6)
+    return 0.5 * (lam * tf.square(label - mu) / (tf.square(mu) * label)
+                  + tf.log(2 * np.pi * tf.pow(label, 3.) - tf.log(lam)))
--- net_building_blocks/losses_test.py
+++ net_building_blocks/losses_test.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 from unittest import TestCase
 import tensorflow as tf
 import numpy as np
-from .losses import exponential_nll_loss
+from .losses import exponential_nll_loss, inverse_gaussian_nll_loss
 
 
 class Test(TestCase):
@@ -22,3 +22,21 @@ class Test(TestCase):
                 print(i, sess.run([train_op, loss, lam]))
 
         print(1 / np.mean(data))
+
+    def test_inverse_gaussian_nll_loss(self):
+
+        tf.reset_default_graph()
+        data = np.random.wald(mean=10, scale=2, size=10000)
+
+        print(data)
+        inp = tf.constant(data, dtype=tf.float32, shape=data.shape)
+        lam = tf.exp(tf.get_variable("lambda", shape=[], dtype=tf.float32))
+        mu = tf.exp(tf.get_variable("mu", shape=[], dtype=tf.float32))
+        loss = tf.reduce_mean(inverse_gaussian_nll_loss(lam, mu, inp))
+        train_op = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(10000):
+                print(i, sess.run([train_op, loss, lam, mu]))
+
+        print(np.mean(data))
